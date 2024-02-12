e297408c8b6b42f6bbd40176c41c6930e64bc6f4
kalleva <kaluzhninlev@gmail.com>
2024-02-12 21:10:54 +0100

    002LEDToggleOD

---
+++ stm32l4xx_drivers/Src/002LEDToggleOD.c
@@ -0,0 +1,19 @@
+#include "stm32l476xx.h"
+#include "stm32l476xx_gpio_driver.h"
+
+int main(void) {
+  GPIO_Handle_t GpioLed = {GPIOA,
+                           {GPIO_PIN_NO_5, GPIO_MODE_OUT, GPIO_SPEED_LOW,
+                            GPIO_OP_TYPE_OD, GPIO_NO_PUPD}};
+
+  /* With external resistor connected between PA5 and VDD */
+  GPIO_PeriClockControl(GPIOA, ENABLE);
+
+  GPIO_Init(&GpioLed);
+
+  while (1) {
+    GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+    for (int i = 0; i < 1000000; i++)
+      ;
+  }
+}
